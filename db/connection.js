fa16994a1411ed726ffee8a1436ebb4db5eceaa6
MrVolcano <paul@mckinley-it.com>
2025-03-22 15:53:16 +0000

    fixed Production seeding issues

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,7 @@
 const { Pool } = require("pg");
 
 const ENV = process.env.NODE_ENV || "development";
+console.log("ENV is :", ENV);
 require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
 
 const db = new Pool();
@@ -11,11 +12,13 @@ if (ENV === "production") {
   config.max = 2;
 }
 
+console.log(config);
+
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
-
 } else {
   console.log(`Connected to ${process.env.PGDATABASE}`);
 }
 
-module.exports = {db, new Pool(config)}
+module.exports = db;
+module.exports = new Pool(config);
